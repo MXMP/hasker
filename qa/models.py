37ea96acbac9861dccba3d57f6b7e9934d684b77
mxmp <bandoftoys@gmail.com>
2019-03-07 19:20:37 +0400

    Накидал каркасы моделек

---
+++ qa/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# TODO: Это вообще зачем? Заюзать стандартную модель
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    # TODO: может придумаем что-то получше?
+    avatar = models.CharField(max_length=100)
+    register_date = models.DateTimeField()
+
+
+class Answer(models.Model):
+    header = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    pub_date = models.DateTimeField()
+
+
+class Question(models.Model):
+    header = models.CharField(max_length=100)
+    text = models.TextField()
+    creation_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    right_answer = models.ForeignKey(Answer, on_delete=models.SET_NULL)
+
+
+class Tag(models.Model):
+    word = models.CharField(max_length=30)
