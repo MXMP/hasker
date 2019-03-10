cbd65a89516891ba7f1d565892059be7be51d834
mxmp <bandoftoys@gmail.com>
2019-03-11 00:01:05 +0400

    В шапке для авторизованного пользователя отображается его ник.

--- qa/urls.py
+++ qa/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+import django.contrib.auth.urls
 
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('user/', include('django.contrib.auth.urls')),
 ]
