bd98101e782683d399f507c101bd74bdb14bc289
mxmp <bandoftoys@gmail.com>
2019-03-10 20:49:01 +0400

    Заработало IndexView

--- qa/admin.py
+++ qa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, User, Answer, Tag
+
+admin.site.register(Question)
+admin.site.register(User)
+admin.site.register(Answer)
+admin.site.register(Tag)
--- qa/models.py
+++ qa/models.py
@@ -7,13 +7,13 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     email = models.EmailField()
     # TODO: может придумаем что-то получше?
-    avatar = models.CharField(max_length=100)
+    avatar = models.CharField(max_length=100, null=True)
     register_date = models.DateTimeField()
 
 
 class Answer(models.Model):
     header = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     pub_date = models.DateTimeField()
 
 
@@ -21,8 +21,11 @@ class Question(models.Model):
     header = models.CharField(max_length=100)
     text = models.TextField()
     creation_date = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.SET_NULL)
-    right_answer = models.ForeignKey(Answer, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    right_answer = models.ForeignKey(Answer, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.header
 
 
 class Tag(models.Model):
