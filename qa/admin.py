507348aca5569e14642f977b36dfe8afa114fbd2
mxmp <bandoftoys@gmail.com>
2019-03-11 22:58:59 +0400

    Связал модельки вопроса и ответа между собой

--- qa/admin.py
+++ qa/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Question, Answer, Tag
 
-admin.site.register(Question)
-admin.site.register(Answer)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('header', 'author', 'pub_date')
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('author', 'for_question', 'pub_date')
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
 admin.site.register(Tag)
--- qa/models.py
+++ qa/models.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Answer(models.Model):
-    header = models.CharField(max_length=100)
+    content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    for_question = models.ForeignKey('qa.Question', on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.author} - {self.for_question} - {self.content[:20]}'
 
 
 class Question(models.Model):
     header = models.CharField(max_length=100)
     text = models.TextField()
-    creation_date = models.DateTimeField()
+    pub_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     right_answer = models.ForeignKey(Answer, on_delete=models.SET_NULL, null=True, blank=True)
 
