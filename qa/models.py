8ecfaece7e008fd080276579eaba68fd88f48be8
mxmp <bandoftoys@gmail.com>
2019-03-12 19:47:48 +0400

    К моделькам вопроса и ответа добавил поля для количества голосов. К модельке тэгов добавил many-to-many поле для связи с моделькой вопроса.

--- qa/models.py
+++ qa/models.py
@@ -7,6 +7,7 @@ class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     for_question = models.ForeignKey('qa.Question', on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.author} - {self.for_question} - {self.content[:20]}'
@@ -18,6 +19,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     right_answer = models.ForeignKey(Answer, on_delete=models.SET_NULL, null=True, blank=True)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.header
@@ -25,3 +27,7 @@ class Question(models.Model):
 
 class Tag(models.Model):
     word = models.CharField(max_length=30)
+    questions = models.ManyToManyField(Question, blank=True)
+
+    def __str__(self):
+        return self.word
