8c9ede3fe303b6c8bd04cd7c56a168cbd015cd94
mxmp <bandoftoys@gmail.com>
2019-03-12 20:35:23 +0400

    Добавил простенькую пагинацию.

--- qa/views.py
+++ qa/views.py
@@ -6,6 +6,7 @@ from .models import Question
 class IndexView(ListView):
     model = Question
     template_name = 'qa/index.html'
+    paginate_by = 20
 
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')
