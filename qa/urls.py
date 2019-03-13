c17bf307c3cd6d1526a7f8790f3238fb2f681c77
mxmp <bandoftoys@gmail.com>
2019-03-14 00:45:28 +0400

    Сделаны рабочими кнопки голосования за вопрос. Есть подозрение, что нужно делать по-другому.

--- qa/urls.py
+++ qa/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('user/', include('django.contrib.auth.urls')),
     path('<int:pk>/', views.QuestionView.as_view(), name='question_view'),
-    path('<int:question_id>/answer', views.answer_question, name='answer_question')
+    path('<int:question_id>/answer', views.answer_question, name='answer_question'),
+    path('<int:question_id>/vote', views.question_vote, name='question_vote'),
 ]
--- qa/views.py
+++ qa/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.db.models import F
 
 from .models import Question
 
@@ -23,4 +24,15 @@ class QuestionView(DetailView):
 def answer_question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST['answer_content'], author=request.user)
+    return HttpResponseRedirect(reverse('qa:question_view', args=(question.id,)))
+
+
+def question_vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.POST['question_vote'] == 'up':
+        question.votes = F('votes') + 1
+        question.save()
+    elif request.POST['question_vote'] == 'down':
+        question.votes = F('votes') - 1
+        question.save()
     return HttpResponseRedirect(reverse('qa:question_view', args=(question.id,)))
