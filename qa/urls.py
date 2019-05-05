e7552378385cf34a0e1890dc04665f261e7b5d77
mxmp <bandoftoys@gmail.com>
2019-05-05 23:40:42 +0400

    Added signup page

--- qa/urls.py
+++ qa/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('ask/', views.AskView.as_view(), name='ask_view'),
     path('user/', include('django.contrib.auth.urls')),
+    path('user/signup/', views.signup, name='signup'),
     path('<int:pk>/', views.QuestionView.as_view(), name='question_view'),
     path('<int:question_id>/answer', views.answer_question, name='answer_question'),
     path('<int:question_id>/vote', views.question_vote, name='question_vote'),
--- qa/views.py
+++ qa/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import authenticate, login
 from django.views.generic import ListView, DetailView, CreateView
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.db.models import F
 
 from .models import Question
+from .forms import SignUpForm
 
 
 class IndexView(ListView):
@@ -43,3 +45,18 @@ def question_vote(request, question_id):
         question.votes = F('votes') - 1
         question.save()
     return HttpResponseRedirect(reverse('qa:question_view', args=(question.id,)))
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('qa:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
