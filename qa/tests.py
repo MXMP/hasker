2c66adf87ea67b077f30d83b556c65c311bb6a01
mxmp <bandoftoys@gmail.com>
2019-05-05 20:30:38 +0400

    Added login page

--- qa/tests.py
+++ qa/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.views import LoginView
 
 
 class IndexViewTests(TestCase):
@@ -11,3 +13,34 @@ class IndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No questions!")
         self.assertQuerysetEqual(response.context['question_list'], [])
+
+
+class LoginViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='test_user', email='test@example.com', password='secret')
+
+    def test_logout_button_for_authenticated_user(self):
+        """
+        Если пользователь уже авторизован, то ему должна показываться кнопка "Logout".
+        """
+        request = self.factory.get(reverse('qa:login'))
+
+        # Simulate logged-in user
+        request.user = self.user
+
+        response = LoginView.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Logout")
+
+    def test_no_logout_button_for_not_authenticated_user(self):
+        """
+        Если пользователь не залогинен, то кнопка "Logout" не должна быть показана.
+        """
+        request = self.factory.get(reverse('qa:login'))
+
+        request.user = AnonymousUser()
+        response = LoginView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Logout")
