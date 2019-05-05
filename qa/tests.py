f4e04605d37411dfd604bfe44fd57b14fd50cf21
mxmp <bandoftoys@gmail.com>
2019-05-05 14:38:18 +0400

    Added test for empty question list

--- qa/tests.py
+++ qa/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def test_no_question(self):
+        """
+        Если нет ни одного вопроса, то пользователь должен видет соответствующее сообщение.
+        """
+        response = self.client.get(reverse('qa:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No questions!")
+        self.assertQuerysetEqual(response.context['question_list'], [])
