e9610ddc8eae016edcfa148f33e1d26b041e5a74
mxmp <bandoftoys@gmail.com>
2019-05-10 19:54:35 +0400

    Added Profile model to extend standard User model.

--- qa/models.py
+++ qa/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.utils import timezone
 from django.urls import reverse
+from django.dispatch import receiver
 
 
 class Answer(models.Model):
@@ -36,3 +38,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.word
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    signup_date = models.DateTimeField(default=timezone.now)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
